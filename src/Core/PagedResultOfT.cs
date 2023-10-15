e39aaa41c3fc3ecd3f22df73b8ecf7d6d967489e
MrDave1999 <daveseva2010@hotmail.es>
2023-10-15 16:23:32 -0500

    feat: Add the `PagedResult<T>` type to represent pagination

---
+++ src/Core/PagedResultOfT.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleResults;
+
+/// <summary>
+/// Represents the paged result of an operation.
+/// </summary>
+/// <typeparam name="T">The type of objects to enumerate.</typeparam>
+public sealed class PagedResult<T> : ResultBase
+{
+    /// <summary>
+    /// Gets the data from a page.
+    /// </summary>
+    public IEnumerable<T> Data { get; init; }
+
+    /// <summary>
+    /// Gets information about the page.
+    /// </summary>
+    public PagedInfo PagedInfo { get; init; }
+
+    public static implicit operator PagedResult<T>(Result result)
+    {
+        return new PagedResult<T>
+        {
+            Data        = Enumerable.Empty<T>(),
+            PagedInfo   = default,
+            IsSuccess   = result.IsSuccess,
+            Message     = result.Message,
+            Errors      = result.Errors,
+            Status      = result.Status
+        };
+    }
+}
