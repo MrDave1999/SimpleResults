de2e68087ddee29bd81e5e50555918eb63026e38
MrDave1999 <daveseva2010@hotmail.es>
2023-10-15 10:28:48 -0500

    tests: Add unit test to `Result<T>`

---
+++ tests/ResultOfT.Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleResults.Tests;
+
+public class ResultOfTTests
+{
+    [Test]
+    public void ImplicitOperator_WhenConvertedFromResultType_ShouldReturnsResultOfT()
+    {
+        // Arrange
+        var expectedMessage = ResponseMessages.NotFound;
+
+        // Act
+        Result<List<Person>> actual = Result.NotFound();
+
+        // Asserts
+        actual.Data.Should().BeNull();
+        actual.IsSuccess.Should().BeFalse();
+        actual.IsFailed.Should().BeTrue();
+        actual.Message.Should().Be(expectedMessage);
+        actual.Errors.Should().BeEmpty();
+        actual.Status.Should().Be(ResultStatus.NotFound);
+    }
+}
