b939b08b081ed2b8c72435c4ec5110d256b02873
MrDave1999 <daveseva2010@hotmail.es>
2023-10-17 12:24:15 -0500

    chore: Create fake users for testing

---
+++ samples/SimpleResults.Example.AspNetCore/DataSeeds.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleResults.Example.AspNetCore;
+
+public class DataSeeds
+{
+    public static List<User> CreateUsers()
+    {
+        var testUsers = new Faker<User>()
+            .RuleFor(u => u.Id, Guid.NewGuid().ToString())
+            .RuleFor(u => u.Name, f => f.Name.FirstName());
+
+        return testUsers.Generate(10);
+    }
+}
