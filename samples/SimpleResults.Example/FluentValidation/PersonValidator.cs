23e89c24c3e32accdae3a542702a9552aeec3e05
MrDave1999 <daveseva2010@hotmail.es>
2023-10-21 12:57:56 -0500

    chore: Add an example with Fluent Validation

---
+++ samples/SimpleResults.Example/FluentValidation/PersonValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleResults.Example.FluentValidation;
+
+public class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(p => p.FirstName).NotEmpty();
+        RuleFor(p => p.LastName).NotEmpty();
+    }
+}
---
+++ samples/SimpleResults.Example/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using FluentValidation;
+global using FluentValidation.Results;
