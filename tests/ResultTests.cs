fb6d6ab668bb2962e462ce267ea636580650da53
MrDave1999 <daveseva2010@hotmail.es>
2023-10-21 12:04:39 -0500

    test: Add partial class for ResultTests type

--- tests/ResultTests.cs
+++ tests/ResultTests.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleResults.Tests;
 
-public class ResultTests
+public partial class ResultTests
 {
     [Test]
     public void Success_WhenResultIsSuccessWithMessage_ShouldReturnsResultOfT()
@@ -266,113 +266,5 @@ public class ResultTests
         actual.Errors.Should().BeEmpty();
         actual.Data.Should().BeEquivalentTo(expectedData);
         actual.Status.Should().Be(ResultStatus.Ok);
-    }
-
-    [Test]
-    public void Failure_WhenResultIsFailure_ShouldReturnsResultObject()
-    {
-        // Arrange
-        var expectedMessage = "Failure";
-        var expectedErrors = new[] { "error" };
-
-        // Act
-        Result actual = Result.Failure(expectedMessage, expectedErrors);
-
-        // Asserts
-        actual.IsSuccess.Should().BeFalse();
-        actual.IsFailed.Should().BeTrue();
-        actual.Message.Should().Be(expectedMessage);
-        actual.Errors.Should().BeEquivalentTo(expectedErrors);
-        actual.Status.Should().Be(ResultStatus.Failure);
-    }
-
-    [Test]
-    public void Invalid_WhenResultIsInvalid_ShouldReturnsResultObject()
-    {
-        // Arrange
-        var expectedMessage = "Invalid";
-        var expectedErrors = new[] { "error" };
-
-        // Act
-        Result actual = Result.Invalid(expectedMessage, expectedErrors);
-
-        // Asserts
-        actual.IsSuccess.Should().BeFalse();
-        actual.IsFailed.Should().BeTrue();
-        actual.Message.Should().Be(expectedMessage);
-        actual.Errors.Should().BeEquivalentTo(expectedErrors);
-        actual.Status.Should().Be(ResultStatus.Invalid);
-    }
-
-    [Test]
-    public void NotFound_WhenResultIsNotFound_ShouldReturnsResultObject()
-    {
-        // Arrange
-        var expectedMessage = "NotFound";
-        var expectedErrors = new[] { "error" };
-
-        // Act
-        Result actual = Result.NotFound(expectedMessage, expectedErrors);
-
-        // Asserts
-        actual.IsSuccess.Should().BeFalse();
-        actual.IsFailed.Should().BeTrue();
-        actual.Message.Should().Be(expectedMessage);
-        actual.Errors.Should().BeEquivalentTo(expectedErrors);
-        actual.Status.Should().Be(ResultStatus.NotFound);
-    }
-
-    [Test]
-    public void Unauthorized_WhenResultIsUnauthorized_ShouldReturnsResultObject()
-    {
-        // Arrange
-        var expectedMessage = "Unauthorized";
-        var expectedErrors = new[] { "error" };
-
-        // Act
-        Result actual = Result.Unauthorized(expectedMessage, expectedErrors);
-
-        // Asserts
-        actual.IsSuccess.Should().BeFalse();
-        actual.IsFailed.Should().BeTrue();
-        actual.Message.Should().Be(expectedMessage);
-        actual.Errors.Should().BeEquivalentTo(expectedErrors);
-        actual.Status.Should().Be(ResultStatus.Unauthorized);
-    }
-
-    [Test]
-    public void Conflict_WhenResultIsConflict_ShouldReturnsResultObject()
-    {
-        // Arrange
-        var expectedMessage = "Conflict";
-        var expectedErrors = new[] { "error" };
-
-        // Act
-        Result actual = Result.Conflict(expectedMessage, expectedErrors);
-
-        // Asserts
-        actual.IsSuccess.Should().BeFalse();
-        actual.IsFailed.Should().BeTrue();
-        actual.Message.Should().Be(expectedMessage);
-        actual.Errors.Should().BeEquivalentTo(expectedErrors);
-        actual.Status.Should().Be(ResultStatus.Conflict);
-    }
-
-    [Test]
-    public void CriticalError_WhenResultIsCriticalError_ShouldReturnsResultObject()
-    {
-        // Arrange
-        var expectedMessage = "CriticalError";
-        var expectedErrors = new[] { "error" };
-
-        // Act
-        Result actual = Result.CriticalError(expectedMessage, expectedErrors);
-
-        // Asserts
-        actual.IsSuccess.Should().BeFalse();
-        actual.IsFailed.Should().BeTrue();
-        actual.Message.Should().Be(expectedMessage);
-        actual.Errors.Should().BeEquivalentTo(expectedErrors);
-        actual.Status.Should().Be(ResultStatus.CriticalError);
     }
 }
