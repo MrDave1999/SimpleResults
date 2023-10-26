5ffe822f2a3da3a093f581b1f5a97a3bda4d4cbe
MrDave1999 <daveseva2010@hotmail.es>
2023-10-26 16:58:42 -0500

    test: Add unit tests for the action filter

---
+++ tests/AspNetCore/TranslateResultToActionResultAttributeTests.cs
@@ -0,0 +1,69 @@
+﻿namespace SimpleResults.Tests.AspNetCore;
+
+public class TranslateResultToActionResultAttributeTests
+{
+    private static ActionExecutedContext CreateContext()
+    {
+        var httpContext = new DefaultHttpContext();
+        var actionContext = new ActionContext(httpContext, new(), new(), new());
+        var filters = new List<IFilterMetadata>();
+        return new ActionExecutedContext(actionContext, filters, controller: null);
+    }
+
+    [Test]
+    public void OnActionExecuted_WhenObjectResultValueIsNotResultBase_ShouldNotRunAnyLogic()
+    {
+        // Arrange
+        var actionExecutedContext = CreateContext();
+        var actionFilter = new TranslateResultToActionResultAttribute();
+        var person = new Person { Name = "Test" };
+        actionExecutedContext.Result = new ObjectResult(person);
+
+        // Act
+        actionFilter.OnActionExecuted(actionExecutedContext);
+
+        // Asserts
+        var objectResult = actionExecutedContext.Result as ObjectResult;
+        objectResult.StatusCode.Should().BeNull();
+        objectResult.Value.Should().As<Person>();
+    }
+
+    [Test]
+    public void OnActionExecuted_WhenObjectResultValueIsResultBase_ShouldTranslateToActionResult()
+    {
+        // Arrange
+        var actionExecutedContext = CreateContext();
+        var actionFilter = new TranslateResultToActionResultAttribute();
+        var person = new Person { Name = "Test" };
+        Result<Person> result = Result.Success(person);
+        actionExecutedContext.Result = new ObjectResult(result);
+
+        // Act
+        actionFilter.OnActionExecuted(actionExecutedContext);
+
+        // Asserts
+        var objectResult = actionExecutedContext.Result as OkObjectResult;
+        objectResult.StatusCode.Should().Be(200);
+        objectResult.Value.Should().As<Result<Person>>();
+    }
+
+    [Test]
+    public void OnActionExecuted_WhenContextResultIsNotObjectResult_ShouldNotRunAnyLogic()
+    {
+        // Arrange
+        var actionExecutedContext = CreateContext();
+        var actionFilter = new TranslateResultToActionResultAttribute();
+        actionExecutedContext.Result = new TestResult();
+
+        // Act
+        actionFilter.OnActionExecuted(actionExecutedContext);
+
+        // Assert
+        actionExecutedContext.Result.Should().As<TestResult>();
+    }
+
+    private class TestResult : IActionResult
+    {
+        public Task ExecuteResultAsync(ActionContext context) => throw new NotImplementedException();
+    }
+}
--- tests/GlobalUsings.cs
+++ tests/GlobalUsings.cs
@@ -3,4 +3,6 @@ global using System.Text.Json;
 global using NUnit.Framework;
 global using FluentAssertions;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc.Filters;
 global using SimpleResults.Resources;
