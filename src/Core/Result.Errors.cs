798063ac66090202c21f4d3cb07ab98c97442dbe
MrDave1999 <daveseva2010@hotmail.es>
2023-11-12 09:52:49 -0500

    style: Add comment to parameter "errors"

--- src/Core/Result.Errors.cs
+++ src/Core/Result.Errors.cs
@@ -26,6 +26,7 @@ public partial class Result
     public static Result Failure(string message) 
         => Failure(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="Failure(string, IEnumerable{string})" />
     public static Result Failure(IEnumerable<string> errors) 
         => Failure(string.Empty, errors);
@@ -47,6 +48,7 @@ public partial class Result
     public static Result Invalid(string message) 
         => Invalid(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="Invalid(string, IEnumerable{string})" />
     public static Result Invalid(IEnumerable<string> errors) 
         => Invalid(ResponseMessages.ValidationErrors, errors);
@@ -69,6 +71,7 @@ public partial class Result
     public static Result NotFound(string message) 
         => NotFound(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="NotFound(string, IEnumerable{string})" />
     public static Result NotFound(IEnumerable<string> errors) 
         => NotFound(string.Empty, errors);
@@ -90,6 +93,7 @@ public partial class Result
     public static Result Unauthorized(string message) 
         => Unauthorized(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="Unauthorized(string, IEnumerable{string})" />
     public static Result Unauthorized(IEnumerable<string> errors) 
         => Unauthorized(string.Empty, errors);
@@ -115,6 +119,7 @@ public partial class Result
     public static Result Conflict(string message) 
         => Conflict(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="Conflict(string, IEnumerable{string})" />
     public static Result Conflict(IEnumerable<string> errors) 
         => Conflict(string.Empty, errors);
@@ -136,12 +141,13 @@ public partial class Result
     public static Result CriticalError(string message) 
         => CriticalError(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="CriticalError(string, IEnumerable{string})" />
     public static Result CriticalError(IEnumerable<string> errors)
         => CriticalError(string.Empty, errors);
 
     /// <summary>
-    /// Represents a critical error that occurred during the execution of the service.
+    /// Represents a situation where a service detects unexpected behavior (critical error) and does not know how to handle it.
     /// </summary>
     /// <remarks>
     /// Everything provided by the user was valid, but the service was unable to complete due to an exception.
@@ -160,6 +166,7 @@ public partial class Result
     public static Result Forbidden(string message)
         => Forbidden(message, Enumerable.Empty<string>());
 
+    /// <param name="errors">A collection of string that contains error messages.</param>
     /// <inheritdoc cref="Forbidden(string, IEnumerable{string})" />
     public static Result Forbidden(IEnumerable<string> errors)
         => Forbidden(string.Empty, errors);
