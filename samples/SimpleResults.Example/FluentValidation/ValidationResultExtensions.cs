b36e91380ff136cf084730d8f949711704e58000
MrDave1999 <daveseva2010@hotmail.es>
2023-11-20 12:56:31 -0500

    chore: Remove ValidationResultExtensions type from sample project

--- samples/SimpleResults.Example/FluentValidation/ValidationResultExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SimpleResults.Example.FluentValidation;
-
-public static class ValidationResultExtensions
-{
-    public static bool IsFailed(this ValidationResult result) => !result.IsValid;
-    public static IEnumerable<string> AsErrors(this ValidationResult result)
-        => result.Errors.Select(failure => failure.ErrorMessage);
-}
