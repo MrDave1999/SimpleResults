57af7ed0745b1b9eb9b0871b88acef9f2bdc1523
MrDave1999 <daveseva2010@hotmail.es>
2023-12-08 18:00:25 -0500

    docs: Add samples about Minimal APIs

--- samples/SimpleResults.Example.AspNetCore/GlobalUsings.cs
+++ samples/SimpleResults.Example.AspNetCore/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using Microsoft.AspNetCore.Mvc;
 global using SimpleResults;
 global using SimpleResults.Example;
 global using SimpleResults.Example.AspNetCore;
+global using SimpleResults.Example.AspNetCore.MinimalApi;
 global using SimpleResults.Example.AspNetCore.Models;
 global using SimpleResults.Example.FluentValidation;
 global using Person = SimpleResults.Example.FluentValidation.Person;
---
+++ samples/SimpleResults.Example.AspNetCore/MinimalApi/UserEndpoint.cs
@@ -0,0 +1,59 @@
+﻿namespace SimpleResults.Example.AspNetCore.MinimalApi;
+
+public static class UserEndpoint
+{
+    public static void AddUserRoutes(this WebApplication app)
+    {
+        var userGroup = app
+            .MapGroup("/User-MinimalApi")
+            .WithTags("User MinimalApi");
+
+        userGroup.MapGet("/paged", ([AsParameters]PagedRequest request, UserService service) =>
+        {
+            return service
+                .GetPagedList(request.PageNumber, request.PageSize)
+                .ToHttpResult();
+        })
+        .Produces<PagedResult<User>>();
+
+        userGroup.MapGet("/", (UserService service) =>
+        {
+            return service
+                .GetAll()
+                .ToHttpResult();
+        })
+        .Produces<ListedResult<User>>();
+
+        userGroup.MapGet("/{id}", (string id, UserService service) =>
+        {
+            return service
+                .GetById(id)
+                .ToHttpResult();
+        })
+        .Produces<Result<User>>();
+
+        userGroup.MapPost("/", ([FromBody]UserRequest request, UserService service) =>
+        {
+            return service
+                .Create(request.Name)
+                .ToHttpResult();
+        })
+        .Produces<Result<CreatedGuid>>();
+
+        userGroup.MapPut("/{id}", (string id, [FromBody]UserRequest request, UserService service) =>
+        {
+            return service
+                .Update(id, request.Name)
+                .ToHttpResult();
+        })
+        .Produces<Result>();
+
+        userGroup.MapDelete("/{id}", (string id, UserService service) =>
+        {
+            return service
+                .Delete(id)
+                .ToHttpResult();
+        })
+        .Produces<Result>();
+    }
+}
