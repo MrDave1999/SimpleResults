a9cbbb99f18774cf4fb2ac12361a7d6167d90433
MrDave1999 <daveseva2010@hotmail.es>
2023-12-09 11:03:24 -0500

    feat: Add implicit conversion from value to Result<T>

--- tests/Core/ResultOfT.Tests.cs
+++ tests/Core/ResultOfT.Tests.cs
@@ -37,4 +37,27 @@ public class ResultOfTTests
         actual.Errors.Should().BeEmpty();
         actual.Status.Should().Be(ResultStatus.NotFound);
     }
+
+    [Test]
+    public void ImplicitOperator_WhenConvertedFromValueOfT_ShouldReturnsResultOfT()
+    {
+        // Arrange
+        var expectedValue = new List<Person>
+        {
+            new() { Name = "Bob" },
+            new() { Name = "Alice" }
+        };
+        var expectedMessage = ResponseMessages.Success;
+
+        // Act
+        Result<List<Person>> actual = expectedValue;
+
+        // Asserts
+        actual.Data.Should().BeEquivalentTo(expectedValue);
+        actual.IsSuccess.Should().BeTrue();
+        actual.IsFailed.Should().BeFalse();
+        actual.Message.Should().Be(expectedMessage);
+        actual.Errors.Should().BeEmpty();
+        actual.Status.Should().Be(ResultStatus.Ok);
+    }
 }
