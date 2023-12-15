011ea707d626030f42f77fd504fa583d4cb8cb07
MrDave1999 <daveseva2010@hotmail.es>
2023-12-15 11:34:37 -0500

    test: Add validators for testing purposes

---
+++ tests/FluentValidation/Validators/OrderDetailValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleResults.Tests.FluentValidation.Validators;
+
+public class OrderDetail
+{
+    public string Product { get; init; }
+    public int Amount { get; init; }
+    public double Price { get; init; }
+}
+
+public class OrderDetailValidator : AbstractValidator<OrderDetail>
+{
+    public OrderDetailValidator()
+    {
+        RuleFor(o => o.Product).NotEmpty();
+        RuleFor(o => o.Amount).GreaterThan(0);
+        RuleFor(o => o.Price).GreaterThan(0);
+    }
+}
---
+++ tests/FluentValidation/Validators/OrderValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleResults.Tests.FluentValidation.Validators;
+
+public class Order
+{
+    public string Customer { get; init; }
+    public string Description { get; init; }
+    public Address DeliveryAddress { get; init; }
+    public IEnumerable<OrderDetail> Details { get; init; }
+}
+
+public class OrderValidator : AbstractValidator<Order>
+{
+    public OrderValidator()
+    {
+        RuleFor(o => o.Customer).NotEmpty();
+        RuleFor(o => o.Description).NotEmpty();
+        RuleFor(o => o.DeliveryAddress)
+            .NotEmpty()
+            .SetValidator(new DeliveryAddressValidator());
+        RuleFor(o => o.Details).NotEmpty();
+        RuleForEach(o => o.Details).SetValidator(new OrderDetailValidator());
+    }
+}
