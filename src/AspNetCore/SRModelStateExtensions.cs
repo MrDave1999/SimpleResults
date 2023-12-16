be8fe3ded2bb2799b08fdab079b3766e5c35bf0d
MrDave1999 <daveseva2010@hotmail.es>
2023-12-16 17:40:57 -0500

    docs: Add XML comments

--- src/AspNetCore/SRModelStateExtensions.cs
+++ src/AspNetCore/SRModelStateExtensions.cs
@@ -5,21 +5,72 @@ using SimpleResults.Resources;
 
 namespace SimpleResults;
 
+/// <summary>
+/// Contains extension methods that convert an instance of <see cref="ModelStateDictionary"/> 
+/// into an instance of type <see cref="Result"/>.
+/// </summary>
 public static class SRModelStateExtensions
 {
+    /// <summary>
+    /// Checks if the model state is failed.
+    /// </summary>
+    /// <param name="modelState">The model state to be validated.</param>
+    /// <returns>
+    /// <c>true</c> if the model state is failed; otherwise <c>false</c>.
+    /// </returns>
     public static bool IsFailed(this ModelStateDictionary modelState)
         => !modelState.IsValid;
 
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>An instance of type <see cref="Result"/> that does not contain a value.</returns>
     public static Result Invalid(this ModelStateDictionary modelState)
         => Result.Invalid(modelState.AsErrors());
 
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <param name="message">An error message.</param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>An instance of type <see cref="Result"/> that does not contain a value.</returns>
     public static Result Invalid(this ModelStateDictionary modelState, string message)
         => Result.Invalid(message, modelState.AsErrors());
 
-    public static ActionResult BadRequest(this ModelStateDictionary modelState)
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>
+    /// An instance of type <see cref="BadRequestObjectResult"/> 
+    /// that contains an instance of type <see cref="Result"/>.
+    /// </returns>
+    public static BadRequestObjectResult BadRequest(this ModelStateDictionary modelState)
         => modelState.BadRequest(ResponseMessages.ValidationErrors);
 
-    public static ActionResult BadRequest(this ModelStateDictionary modelState, string message)
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <param name="message">An error message.</param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>
+    /// An instance of type <see cref="BadRequestObjectResult"/> 
+    /// that contains an instance of type <see cref="Result"/>.
+    /// </returns>
+    public static BadRequestObjectResult BadRequest(this ModelStateDictionary modelState, string message)
     {
         Result result = Result.Invalid(message, modelState.AsErrors());
         return new BadRequestObjectResult(result);
