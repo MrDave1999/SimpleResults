44d2ffe5565e9326093b8588798a64b8393e91be
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2023-12-17 15:27:11 -0500

    feat: Add extension methods to the ModelStateDictionary type (#56)

---
+++ src/AspNetCore/SRModelStateDictionaryExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SimpleResults.Resources;
+
+namespace SimpleResults;
+
+/// <summary>
+/// Contains extension methods that convert an instance of <see cref="ModelStateDictionary"/> 
+/// into an instance of type <see cref="Result"/>.
+/// </summary>
+public static class SRModelStateDictionaryExtensions
+{
+    /// <summary>
+    /// Checks if the model state is failed.
+    /// </summary>
+    /// <param name="modelState">The model state to be validated.</param>
+    /// <returns>
+    /// <c>true</c> if the model state is failed; otherwise <c>false</c>.
+    /// </returns>
+    public static bool IsFailed(this ModelStateDictionary modelState)
+        => !modelState.IsValid;
+
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>An instance of type <see cref="Result"/> that does not contain a value.</returns>
+    public static Result Invalid(this ModelStateDictionary modelState)
+        => Result.Invalid(modelState.AsErrors());
+
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <param name="message">An error message.</param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>An instance of type <see cref="Result"/> that does not contain a value.</returns>
+    public static Result Invalid(this ModelStateDictionary modelState, string message)
+        => Result.Invalid(message, modelState.AsErrors());
+
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>
+    /// An instance of type <see cref="BadRequestObjectResult"/> 
+    /// that contains an instance of type <see cref="Result"/>.
+    /// </returns>
+    public static BadRequestObjectResult BadRequest(this ModelStateDictionary modelState)
+        => modelState.BadRequest(ResponseMessages.ValidationErrors);
+
+    /// <summary>
+    /// Represents a validation error that prevents the controller action from completing.
+    /// </summary>
+    /// <param name="modelState">
+    /// The <see cref="ModelStateDictionary"/> containing errors to be returned to the client.
+    /// </param>
+    /// <param name="message">An error message.</param>
+    /// <remarks>Used in situations where the consumer provides invalid data.</remarks>
+    /// <returns>
+    /// An instance of type <see cref="BadRequestObjectResult"/> 
+    /// that contains an instance of type <see cref="Result"/>.
+    /// </returns>
+    public static BadRequestObjectResult BadRequest(this ModelStateDictionary modelState, string message)
+    {
+        Result result = Result.Invalid(message, modelState.AsErrors());
+        return new BadRequestObjectResult(result);
+    }
+
+    private static IEnumerable<string> AsErrors(this ModelStateDictionary modelState)
+    {
+        foreach (var(propertyName, value) in modelState)
+        {
+            var errors = value
+                .Errors
+                .Select(model => GetErrorMessage(propertyName, model.ErrorMessage));
+
+            foreach (var error in errors)
+                yield return error;
+        }
+    }
+
+    private static string GetErrorMessage(string propertyName, string message)
+        => string.Format(
+            ResponseMessages.PropertyFailedValidation,
+            propertyName,
+            message);
+}
---
+++ tests/AspNetCore/ModelStateDictionaryExtensionsTests.cs
@@ -0,0 +1,153 @@
+﻿namespace SimpleResults.Tests.AspNetCore;
+
+public class ModelStateDictionaryExtensionsTests
+{
+    [Test]
+    public void IsFailed_WhenModelStateIsInvalid_ShouldReturnsTrue()
+    {
+        // Arrange
+        var modelState = new ModelStateDictionary();
+        modelState.AddModelError("Id", "Id is required.");
+
+        // Act
+        bool actual = modelState.IsFailed();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+
+    [Test]
+    public void IsFailed_WhenModelStateIsValid_ShouldReturnsFalse()
+    {
+        // Arrange
+        var modelState = new ModelStateDictionary();
+
+        // Act
+        bool actual = modelState.IsFailed();
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+
+    [Test]
+    public void Invalid_WhenResultIsInvalidWithoutMessage_ShouldReturnsResultObject()
+    {
+        // Arrange
+        var expectedMessage = ResponseMessages.ValidationErrors;
+        var modelState = new ModelStateDictionary();
+        modelState.AddModelError("Id",   "Id is required.");
+        modelState.AddModelError("Id",   "Id must be greater than 0.");
+        modelState.AddModelError("Name", "Name is required.");
+        modelState.AddModelError("Name", "Name must have a minimum of 10 characters.");
+        var expectedErrors = new[]
+        {
+            "'Id' property failed validation. Error was: Id is required.",
+            "'Id' property failed validation. Error was: Id must be greater than 0.",
+            "'Name' property failed validation. Error was: Name is required.",
+            "'Name' property failed validation. Error was: Name must have a minimum of 10 characters."
+        };
+
+        // Act
+        Result actual = modelState.Invalid();
+
+        // Asserts
+        actual.IsSuccess.Should().BeFalse();
+        actual.IsFailed.Should().BeTrue();
+        actual.Message.Should().Be(expectedMessage);
+        actual.Errors.Should().BeEquivalentTo(expectedErrors);
+        actual.Status.Should().Be(ResultStatus.Invalid);
+    }
+
+    [Test]
+    public void Invalid_WhenResultIsInvalidWithMessage_ShouldReturnsResultObject()
+    {
+        // Arrange
+        var expectedMessage = "Error";
+        var modelState = new ModelStateDictionary();
+        modelState.AddModelError("Id",   "Id is required.");
+        modelState.AddModelError("Id",   "Id must be greater than 0.");
+        modelState.AddModelError("Name", "Name is required.");
+        modelState.AddModelError("Name", "Name must have a minimum of 10 characters.");
+        var expectedErrors = new[]
+        {
+            "'Id' property failed validation. Error was: Id is required.",
+            "'Id' property failed validation. Error was: Id must be greater than 0.",
+            "'Name' property failed validation. Error was: Name is required.",
+            "'Name' property failed validation. Error was: Name must have a minimum of 10 characters."
+        };
+
+        // Act
+        Result actual = modelState.Invalid(expectedMessage);
+
+        // Asserts
+        actual.IsSuccess.Should().BeFalse();
+        actual.IsFailed.Should().BeTrue();
+        actual.Message.Should().Be(expectedMessage);
+        actual.Errors.Should().BeEquivalentTo(expectedErrors);
+        actual.Status.Should().Be(ResultStatus.Invalid);
+    }
+
+    [Test]
+    public void BadRequest_WhenResultIsInvalidWithoutMessage_ShouldReturnsBadRequestObjectResult()
+    {
+        // Arrange
+        int expectedStatus = StatusCodes.Status400BadRequest;
+        var expectedMessage = ResponseMessages.ValidationErrors;
+        var modelState = new ModelStateDictionary();
+        modelState.AddModelError("Id",   "Id is required.");
+        modelState.AddModelError("Id",   "Id must be greater than 0.");
+        modelState.AddModelError("Name", "Name is required.");
+        modelState.AddModelError("Name", "Name must have a minimum of 10 characters.");
+        var expectedErrors = new[]
+        {
+            "'Id' property failed validation. Error was: Id is required.",
+            "'Id' property failed validation. Error was: Id must be greater than 0.",
+            "'Name' property failed validation. Error was: Name is required.",
+            "'Name' property failed validation. Error was: Name must have a minimum of 10 characters."
+        };
+
+        // Act
+        BadRequestObjectResult objectResult = modelState.BadRequest();
+        Result actual = objectResult.Value as Result;
+
+        // Asserts
+        objectResult.StatusCode.Should().Be(expectedStatus);
+        actual.IsSuccess.Should().BeFalse();
+        actual.IsFailed.Should().BeTrue();
+        actual.Message.Should().Be(expectedMessage);
+        actual.Errors.Should().BeEquivalentTo(expectedErrors);
+        actual.Status.Should().Be(ResultStatus.Invalid);
+    }
+
+    [Test]
+    public void BadRequest_WhenResultIsInvalidWithMessage_ShouldReturnsBadRequestObjectResult()
+    {
+        // Arrange
+        int expectedStatus = StatusCodes.Status400BadRequest;
+        var expectedMessage = "Error";
+        var modelState = new ModelStateDictionary();
+        modelState.AddModelError("Id",   "Id is required.");
+        modelState.AddModelError("Id",   "Id must be greater than 0.");
+        modelState.AddModelError("Name", "Name is required.");
+        modelState.AddModelError("Name", "Name must have a minimum of 10 characters.");
+        var expectedErrors = new[]
+        {
+            "'Id' property failed validation. Error was: Id is required.",
+            "'Id' property failed validation. Error was: Id must be greater than 0.",
+            "'Name' property failed validation. Error was: Name is required.",
+            "'Name' property failed validation. Error was: Name must have a minimum of 10 characters."
+        };
+
+        // Act
+        BadRequestObjectResult objectResult = modelState.BadRequest(expectedMessage);
+        Result actual = objectResult.Value as Result;
+
+        // Asserts
+        objectResult.StatusCode.Should().Be(expectedStatus);
+        actual.IsSuccess.Should().BeFalse();
+        actual.IsFailed.Should().BeTrue();
+        actual.Message.Should().Be(expectedMessage);
+        actual.Errors.Should().BeEquivalentTo(expectedErrors);
+        actual.Status.Should().Be(ResultStatus.Invalid);
+    }
+}
