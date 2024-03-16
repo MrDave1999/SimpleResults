8df856a6d5e4cda651f049985e1c12488c396bf2
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2024-03-16 15:48:10 -0500

    docs: Add sample project about native AOT (#68)

---
+++ samples/SimpleResults.Example.NativeAOT/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using SimpleResults;
+global using SimpleResults.Example;
+global using SimpleResults.Example.NativeAOT;
+global using System.Globalization;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
