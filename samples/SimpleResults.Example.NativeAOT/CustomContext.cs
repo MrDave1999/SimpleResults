9a04a9d4f854438bcb9ae7a34723e0c322e70474
MrDave1999 <daveseva2010@hotmail.es>
2024-03-16 15:42:29 -0500

    docs: Add sample project about native AOT

---
+++ samples/SimpleResults.Example.NativeAOT/CustomContext.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleResults.Example.NativeAOT;
+
+[JsonSourceGenerationOptions(
+    WriteIndented = true, 
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(ListedResult<User>))]
+public partial class CustomContext : JsonSerializerContext { }
---
+++ samples/SimpleResults.Example.NativeAOT/Program.cs
@@ -0,0 +1,27 @@
+﻿Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en");
+var users = new List<User>
+{
+    new() { Id = "1", Name = "Bob" },
+    new() { Id = "2", Name = "Alice" }
+};
+var userService = new UserService(users);
+ListedResult<User> result = userService.GetAll();
+
+var json = JsonSerializer.Serialize(
+    result, 
+    CustomContext.Default.ListedResultUser);
+
+Console.WriteLine(json);
+
+var deserializedResult = JsonSerializer.Deserialize(
+    json,
+    CustomContext.Default.ListedResultUser);
+
+Console.WriteLine();
+Console.WriteLine("Status: " + deserializedResult.Status);
+foreach(var user in deserializedResult.Data)
+{
+    Console.WriteLine($"Id: {user.Id}, Name: {user.Name}");
+}
+
+Console.ReadLine();
